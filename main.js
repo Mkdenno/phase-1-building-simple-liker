b3982e95d4be859cdda280f125c144549bf7fb23
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-15 20:54:29 +0300

    Done

--- main.js
+++ main.js
@@ -4,8 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const liker=document.querySelectorAll(".like-glyph")
+const error=document.querySelector('#modal')
+
+for(const likes of liker){
+  liker.addEventListerner('click', ()=>{
+    mimicServerCall(url="http://mimicServer.example.com")
+    .then(()=>{
+      if(liker===EMPTY_HEART){
+        liker.className=".activated-heart";
+      }
+      else{
+        liker.innerText===EMPTY_HEART;
+      }
+    })
+    .catch((message)=>{
+      error.className="";
+      error.innerText=message;
+      setTimeout(()=>{
+        error.className="hidden", 3000;
+      })
+    })
 
 
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
